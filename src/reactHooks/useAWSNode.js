61edd8989b3fe749e14c3abdba3e3998c74decc1
Thomas Haferlach <t.haferlach@gmail.com>
2022-07-08 15:51:39 +0200

    add useAWSNode hook

---
+++ src/reactHooks/useAWSNode.js
@@ -0,0 +1,35 @@
+import Debug from "debug";
+import { useEffect, useState } from "react";
+import { subscribeCID }  from  "@pollinations/ipfs/ipfsPubSub"
+import { submitToAWS } from "../aws";
+
+const debug = Debug("useAWSNode");
+
+
+
+const useAWSNode = ({ nodeID: paramsNodeID } ) => {
+
+    const [nodeID, setNodeID] = useState(paramsNodeID);
+    const [contentID, setContentID] = useState(null);
+
+    // set node ID to the node ID from URL
+    useEffect(() => setNodeID(paramsNodeID), [paramsNodeID])
+
+    // subscribe to content from node
+    useEffect(() => {
+
+        if (!nodeID) return
+
+        // Update
+        debug("nodeID changed to", nodeID, ". (Re)subscribing")
+        const closeSub = subscribeCID(nodeID, "/output", setContentID)
+
+        return closeSub
+
+    }, [nodeID])
+
+    return { nodeID, contentID, setContentID, connected: true, submitToAWS }
+
+};
+
+export default useAWSNode
