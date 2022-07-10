bd84e2cae87d93504182d4311412a7a112836aa0
Thomas Haferlach <t.haferlach@gmail.com>
2022-07-10 19:30:56 +0200

    Add awsModelRunner

---
+++ src/awsModelRunner.js
@@ -0,0 +1,39 @@
+
+import { submitToAWS } from './aws.js';
+import { writer } from './ipfsConnector.js';
+import { subscribeGenerator } from './ipfsPubSub.js';
+import { IPFSWebState } from './ipfsWebClient.js';
+
+const runModel = async (inputs, model="voodoohop/dalle-playground") => {
+    
+    console.log("!!!!submitted prompt", prompt)
+    const inputWriter = writer();
+    const response = await submitToAWS(inputs, inputWriter, model, false)
+  
+    console.log("got pollen id from aws", response)
+    const { nodeID } = response
+  
+    const [cids, unsubscribe] = subscribeGenerator(nodeID, "/output");
+  
+    for await (const cid of cids) {
+    
+      console.log("!!!!received response",cid)
+      const data = await IPFSWebState(cid);
+      console.log("!!!!received response", data)
+    
+      if (data?.output?.done) {
+          unsubscribe()
+          console.log("unsubscribed")
+          
+          const outputEntries = Object.entries(data?.output)
+          
+          // find the first entry whose key ends with .png
+          const [_filename, url] = outputEntries.find(([key]) => key.endsWith(".png"))
+          
+          return url
+      }
+    }
+  
+}
+
+export default runModel;
